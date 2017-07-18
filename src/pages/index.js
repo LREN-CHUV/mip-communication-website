9f1804720a886b272196cd3957d2647186de2c67
James Nolan <j.nolan@otherwise.ch>
2017-07-18 09:34:38 +0200

    Add airbnb react linter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,56 @@
 import React from 'react'
 
-export default () => <div>Hello world2!</div>
+class IndexPage extends React.Component {
+  render() {
+    // const usProductEdges = this.props.data.us.edges
+    // const deProductEdges = this.props.data.german.edges
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query PageQuery {
+    us: allContentfulProduct(filter: { node_locale: { eq: "en-US" } }) {
+      edges {
+        node {
+          id
+          productName {
+            productName
+          }
+          image {
+            responsiveResolution(width: 75) {
+              src
+              srcSet
+              height
+              width
+            }
+          }
+        }
+      }
+    }
+    german: allContentfulProduct(filter: { node_locale: { eq: "de" } }) {
+      edges {
+        node {
+          id
+          productName {
+            productName
+          }
+          image {
+            responsiveResolution(width: 75) {
+              src
+              srcSet
+              height
+              width
+            }
+          }
+        }
+      }
+    }
+  }
+`
