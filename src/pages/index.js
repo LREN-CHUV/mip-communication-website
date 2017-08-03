5208f8db1025b77c27e7ab240d44159be850e2aa
James Nolan <james.nolan@chuv.ch>
2017-08-03 18:07:21 +0200

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,5 @@ import './Index.css'
 // This is also known as the "Landing page". It is called "Index.js" to fit Gatsby's convention
 
 export default () => (
-  <p className="hello">Hello</p>
+  <p>Landing</p>
 )
