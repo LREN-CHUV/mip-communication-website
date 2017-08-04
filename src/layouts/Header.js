834eaeb3deae5949391569afa519a64789618d8b
James Nolan <james.nolan@chuv.ch>
2017-08-04 14:47:45 +0200

    CSS Second level menus

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -19,6 +19,53 @@ const HBPRainbowLine = () => (
   </div>
 )
 
+// The top navigation is a two levels menu. When the user hovers an item
+// from the first level, the second menu appears (if exists). This is done
+// in CSS only
+const TopNavigation = () => (
+  <div role="navigation">
+    <ul className="top-navigation clear">
+      <li className="align-left">
+        <Link to="/" className="bullet">Home</Link>
+      </li>
+      <li className="align-left">
+        <a href="#" className="bullet">Overview</a>
+        <ul className="top-navigation-level-2">
+          <li><Link to="/about-us">About us</Link></li>
+          <li><Link to="/how-it-works">How it works</Link></li>
+        </ul>
+      </li>
+      <li className="align-left">
+        <span className="bullet">Health partners</span>
+        <ul className="top-navigation-level-2">
+          <li className="bullet"><Link to="/hospitals">Hospitals</Link></li>
+          <li className="bullet"><Link to="/cohorts">Cohorts</Link></li>
+        </ul>
+      </li>
+      <li className="align-left">
+        <Link to="/documentation" className="bullet">Documentation</Link>
+      </li>
+      <li className="align-left">
+        <Link to="/news-and-events" className="bullet">News and events</Link>
+      </li>
+      <li className="align-left">
+        <Link to="/contact" className="bullet">Contact</Link>
+      </li>
+      <li className="align-right">
+        <a
+          href="https://mip.humanbrainproject.eu"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button"
+        >
+          <div className="main-text">Login</div>
+          <div className="secondary-text">to platform</div>
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
 export default () => (
   <div>
     <div className="header">
@@ -33,42 +80,6 @@ export default () => (
     </div>
 
     <HBPRainbowLine />
-
-    <div role="navigation">
-      <ul className="top-navigation">
-        <li className="bullet align-left">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="bullet align-left">
-          <Link to="/overview">Overview</Link>
-        </li>
-        <li className="bullet align-left">
-          <Link to="/how-it-works">How it works</Link>
-        </li>
-        <li className="bullet align-left">
-          <Link to="/health-partners">Health partners</Link>
-        </li>
-        <li className="bullet align-left">
-          <Link to="/documentation">Documentation</Link>
-        </li>
-        <li className="bullet align-left">
-          <Link to="/news-and-events">News and events</Link>
-        </li>
-        <li className="bullet align-left">
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li className="align-right">
-          <a
-            href="https://mip.humanbrainproject.eu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button"
-          >
-            <div className="main-text">Login</div>
-            <div className="secondary-text">to platform</div>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <TopNavigation />
   </div>
 )
