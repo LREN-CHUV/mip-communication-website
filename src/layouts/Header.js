7eea1a63d88042e751867c998052d50bebec25cf
James Nolan <james.nolan@chuv.ch>
2017-08-04 13:57:46 +0200

    Factorize HBP colored line out of main component for readability

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -3,6 +3,22 @@ import Link from 'gatsby-link'
 import hbpLogo from '../assets/HBP_logo.svg'
 import './Header.css'
 
+// A purely presentational colored line that appears between the header image
+// and the navigation. It is composed by the main colors of the Humain Brain Project
+// theme (see humanbrainproject.eu)
+const HBPRainbowLine = () => (
+  <div role="presentation">
+    <div className="hbp-rainbow-color bg-red" />
+    <div className="hbp-rainbow-color bg-orange" />
+    <div className="hbp-rainbow-color bg-yellow" />
+    <div className="hbp-rainbow-color bg-blue" />
+    <div className="hbp-rainbow-color bg-half-baked" />
+    <div className="hbp-rainbow-color bg-blue-dark" />
+    <div className="hbp-rainbow-color bg-blue-darker" />
+    <div className="hbp-rainbow-color bg-horizon" />
+  </div>
+)
+
 export default () => (
   <div>
     <div className="header">
@@ -15,16 +31,9 @@ export default () => (
         BETWEEN BRAIN SCIENCE RESEARCH AND CLINICAL PRACTICE
       </div>
     </div>
-    <div role="presentation">
-      <div className="hbp-rainbow-color bg-red" />
-      <div className="hbp-rainbow-color bg-orange" />
-      <div className="hbp-rainbow-color bg-yellow" />
-      <div className="hbp-rainbow-color bg-blue" />
-      <div className="hbp-rainbow-color bg-half-baked" />
-      <div className="hbp-rainbow-color bg-blue-dark" />
-      <div className="hbp-rainbow-color bg-blue-darker" />
-      <div className="hbp-rainbow-color bg-horizon" />
-    </div>
+
+    <HBPRainbowLine />
+
     <div role="navigation">
       <ul className="top-navigation">
         <li className="bullet align-left"><Link to="/">Home</Link></li>
