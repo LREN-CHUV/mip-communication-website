d9e9f3efd0920f98f10d1f2c55c974cf788c823f
James Nolan <james.nolan@chuv.ch>
2017-08-07 11:35:30 +0200

    Health partners overview page

--- src/pages/health-partners.js
+++ src/pages/health-partners.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import './health-partners.css'
 
 
@@ -14,5 +15,33 @@ export default () => (
       </p>
       <p>Five European hospitals and research cohorts have already been recruited.</p>
     </section>
+    <div className="two columns clear">
+      <div className="item">
+        <h2 className="bullet bullet-outline bullet-border-orange large">Hospitals</h2>
+        <p>
+          The first version of the Medical Informatics Platform (MIP) was released in 2016. It
+          enables interactive access to brain-related medical information hosted by European hospitals
+          and researach centers, and facilitates collaboration on analyses and results towards an
+          understanding of brain diseases and neurological disorders.
+        </p>
+        <p>Five European hospitals and research cohorts have already been recruited.</p>
+        <Link to="/health-partners/hospitals" className="button">
+          View partner hospitals
+        </Link>
+      </div>
+      <div className="item">
+        <h2 className="bullet bullet-outline bullet-border-orange large">Large cohorts</h2>
+        <p>
+          The first version of the Medical Informatics Platform (MIP) was released in 2016. It
+          enables interactive access to brain-related medical information hosted by European hospitals
+          and researach centers, and facilitates collaboration on analyses and results towards an
+          understanding of brain diseases and neurological disorders.
+        </p>
+        <p>Five European hospitals and research cohorts have already been recruited.</p>
+        <Link to="/health-partners/hospitals" className="button">
+          View partner cohorts
+        </Link>
+      </div>
+    </div>
   </div>
 )
