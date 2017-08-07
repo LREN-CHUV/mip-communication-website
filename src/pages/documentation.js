593f6f4418fe862589aa0e75800b6f1ae52fcde2
James Nolan <james.nolan@chuv.ch>
2017-08-07 18:13:19 +0200

    Documentation page

---
+++ src/pages/documentation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './documentation.css'
+
+export default () =>
+  <div>
+    <h1 className="bullet bullet-outline bullet-border-red">Documentation</h1>
+    <ul>
+      <li>
+        <a target="_blank" rel="noopener noreferrer" href="https://hbpmedical.github.io/">
+          Technical documentation
+        </a>
+      </li>
+    </ul>
+  </div>
