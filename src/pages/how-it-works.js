8200b330151682c5ad60dc82a5a9cc4f72bea80e
James Nolan <james.nolan@chuv.ch>
2017-08-07 10:55:06 +0200

    How it works page layout

---
+++ src/pages/how-it-works.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './how-it-works.css'
+
+
+export default () => (
+  <div>
+    <div className="breadcrumbs-title-container clear">
+      <span className="breadcrumbs bullet bullet-border-red bullet-outline">
+        Overview &gt;&nbsp;
+      </span>
+      <h1 className="breadcrumbs-title">How it works</h1>
+    </div>
+    <div className="clear">
+      <div className="content-paragraphs">
+        <section>
+          <p>
+            Thounsands of brain images and terabytes of invaluable associated medical data are
+            produced every day at a gigantic pace around the world. the <strong>medical Informatics
+            Platform (MIP)</strong>Y aims to federate this information and provide the tools to
+            the experts to effectively analyse it and advance more rapidly in understanding the
+            neurological and psychiatric diseases. This will in turn allow <strong>identifying
+            the biological changes</strong> associated and open real possibilities for <strong>early
+            diagnosis, personalised treatments and preventive medicine</strong>.
+          </p>
+          <p>
+            thounsands of brain images and terbytes of invaluable associated medical data are
+            produced every day at a gigantic pace around the world. the <strong>medical Informatics
+            Platform (MIP)</strong>Y aims to federate this information and provide the tools to
+            the experts to effectively analyse it and advance more rapidly in understanding the
+            neurological and psychiatric diseases. This will in turn allow <strong>identifying
+            the biological changes</strong> associated and open real possibilities for <strong>early
+            diagnosis, personalised treatments and preventive medicine</strong>.
+          </p>
+        </section>
+      </div>
+      <div className="content-picture">
+        <img src="http://lorempixel.com/600/450/business/" alt="Placeholder" />
+      </div>
+    </div>
+  </div>
+)
