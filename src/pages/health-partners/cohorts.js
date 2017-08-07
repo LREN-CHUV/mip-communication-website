58e51a25d9e468be1833d65083b82923ca4a94b0
James Nolan <james.nolan@chuv.ch>
2017-08-07 13:59:46 +0200

    Cohorts

---
+++ src/pages/health-partners/cohorts.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import './hospitals.css'
+
+export default () =>
+  <div>
+    <div className="breadcrumbs-title-container clear">
+      <span className="breadcrumbs bullet bullet-border-red bullet-outline">
+        Health partners &gt;&nbsp;
+      </span>
+      <h1 className="breadcrumbs-title">Cohorts</h1>
+    </div>
+    <article className="two columns clear">
+      <section className="item wider">
+        <h2 className="bullet bullet-bg-orange no-margin">CREACTIVE</h2>
+        <p>
+          Through the newly established CREACTIVE project, the PROSAFE consortium focuses attention
+          on TBI, on which data collection started in March 2014. Traumatic brain injury (TBI) is
+          among the leading causes of death and disability and the main cause of death among the
+          under-45s.
+        </p>
+        <p>
+          The aims of the project are to consolidate the existing network in order to better
+          describe the epidemiology of moderate-severe TBI in 7 countries (Greece and Israel have
+          now joined the consortium); build a prognostic model based on short- and long-term outcome
+          measures; identify the most effective clinical interventions for optimally treating TBI
+          patients, and recognize the determinants of optimal vs suboptimal performance.
+        </p>
+      </section>
+      <div className="item narrower">
+        <img
+          src="http://lorempixel.com/600/450/business/"
+          className="full-width"
+          alt="Placeholder"
+        />
+      </div>
+    </article>
+
+    <article className="two columns clear">
+      <div className="item narrower">
+        <img
+          src="http://lorempixel.com/600/450/business/"
+          className="full-width"
+          alt="Placeholder"
+        />
+      </div>
+      <section className="item wider">
+        <h2 className="bullet bullet-bg-orange no-margin">EHDN</h2>
+        <div className="subtitle">European Huntington's Ddisease Network</div>
+        <p>
+          The EHDN is a nonprofit research network committed to advancing research, facilitating the
+          conduct of clinical trials, and improving clinical care in HD. Through the EHDN a platform
+          has been created such that basic scientists, clinicians, patients and families can
+          collaborate on academic and industry studies to fulfil its mission. The EHDN is supported
+          by and collaborates closely with CHDI Foundation, Inc.
+        </p>
+      </section>
+    </article>
+
+    <article className="two columns clear">
+      <section className="item wider">
+        <h2 className="bullet bullet-bg-orange no-margin">IMI</h2>
+        <div className="subtitle">Innovative Medicine Initiative</div>
+        <p>
+          The Innovative Medicines Initiative (IMI) is Europe&#39;s largest public-private
+          initiative aiming to speed up the development of better and safer medicines for patients.
+        </p>
+        <p>
+          IMI supports collaborative research projects and builds networks of industrial and
+          academic experts in order to boost pharmaceutical innovation in Europe.
+        </p>
+      </section>
+      <div className="item narrower">
+        <img
+          src="http://lorempixel.com/600/450/business/"
+          className="full-width"
+          alt="Placeholder"
+        />
+      </div>
+    </article>
+  </div>
