b09bcb1f53f074eb87d491fe10d1ff8b06bf8ebe
Ludovic Claude <ludovic.claude54@gmail.com>
2017-11-10 17:43:34 +0100

    [tmp] Disable contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,5 @@ module.exports = {
     title: 'MIP — Human Brain Project',
   },
   plugins: [
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: 'u61ykxdyhywa',
-        accessToken: 'f690447fc2c2487543361d77354fe0c2a85bb12100c9b6ca0fe905525ec35315',
-      },
-    },
   ],
 }
