8a5e475844445738aebd95d8e3603845e8d74383
Panbaba9n <goldgraalar@gmail.com>
2018-02-06 12:06:28 +0200

    heroku demo witout public

--- public/path---health-partners-hospitals-a0e39f21c11f6a62c5ab.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xe5427900b29c],{335:function(t,e){t.exports={pathContext:{}}}});
-//# sourceMappingURL=path---health-partners-hospitals-a0e39f21c11f6a62c5ab.js.map
--- public/path---overview-how-it-works-a0e39f21c11f6a62c5ab.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x8318e098f158],{340:function(t,e){t.exports={pathContext:{}}}});
-//# sourceMappingURL=path---overview-how-it-works-a0e39f21c11f6a62c5ab.js.map
